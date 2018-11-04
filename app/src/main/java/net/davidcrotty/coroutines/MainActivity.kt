ea7d0435c5b64ff09d167eb9409183ac14f9ed19
David Crotty <david.crotty.work@gmail.com>
2018-11-04 15:48:49 +0000

    Runs co-routine on main thread for delayed reaction

--- app/src/main/java/net/davidcrotty/coroutines/MainActivity.kt
+++ app/src/main/java/net/davidcrotty/coroutines/MainActivity.kt
@@ -2,6 +2,10 @@ package net.davidcrotty.coroutines
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Objective: Run some co-routines.
@@ -11,8 +15,27 @@ import android.os.Bundle
  */
 class MainActivity : AppCompatActivity() {
 
+    val job = Job()
+    val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main //just this works, is it sharing //w the UI thread?
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainThreadWorkSliced()
+        Toast.makeText(this, "Act launched", Toast.LENGTH_SHORT).show()
+    }
+
+    fun mainThreadWorkSliced() {
+        GlobalScope.launch(coroutineContext) { //background thread pool, main by default!
+            Log.d("scope", "Thread: ${Thread.currentThread().name}") //should be background
+            delay(5000) // has suspended this portion of the thread, but is running on the main thread. This is a fibre
+            return@launch Toast.makeText(this@MainActivity, "GlobalScope", Toast.LENGTH_SHORT).show() //yield is ui thread
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        job.cancel()
     }
 }
